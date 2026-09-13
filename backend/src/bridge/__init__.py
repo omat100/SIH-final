c8027740d106a9056dd86cd5f06518e6394ffaa2
omat100 <omanandthakur@gmail.com>
2026-09-13 09:01:57 +0530

    testing routes

---
+++ backend/src/bridge/__init__.py
@@ -0,0 +1,5 @@
+"""
+Here we do basically what is discussed in workflow esp to flask 
+We do stuff till pyserial or flask socket io
+But this code cannot be tested by me as i have no equipement
+"""
---
+++ backend/src/bridge/serial_bridge.py
@@ -0,0 +1,30 @@
+import serial
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+esp32 = serial.Serial("COM7", 115200)
+
+def read_serial():
+    while True:
+        data = esp32.readline().decode().strip()
+
+        temperature, humidity, tilt = map(float, data.split(","))
+
+        sensor_data = {
+            "temperature": temperature,
+            "humidity": humidity,
+            "tilt": tilt
+        }
+
+        socketio.emit("sensor_data", sensor_data)
+
+@socketio.on("connect")
+def connected():
+    print("Client connected")
+
+if __name__ == "__main__":
+    socketio.start_background_task(read_serial)
+    socketio.run(app, debug=True)
